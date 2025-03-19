33ad0dd9aade40654ea63ff0456c6b38bb4bc962
miezlearning <gottfriedemptiness@gmail.com>
2025-03-19 14:19:47 +0800

    feat(helper-script): refine folder management script to focus on BOX folders and enhance user prompts

--- helper_imb.bat
+++ helper_imb.bat
@@ -4,22 +4,26 @@ setlocal EnableDelayedExpansion
 :SCAN_FOLDER
 echo Scanning folders...
 set "count=0"
-for /d %%i in (*) do (
+for /d %%i in (BOX*) do (
     set /a count+=1
     set "folder[!count!]=%%i"
-    set "foldername[!count!]=%%i"
+    :: Bersihkan suffix untuk foldername
+    set "clean_name=%%i"
+    set "clean_name=!clean_name: (Belum)=!"
+    set "clean_name=!clean_name: (Done)=!"
+    set "foldername[!count!]=!clean_name!"
 )
 
 if %count% equ 0 (
-    echo No folders found in current directory.
+    echo No BOX folders found in current directory.
 )
 
 :MENU
 cls
-echo Found %count% folders:
+echo Found %count% BOX folders:
 echo.
 for /l %%i in (1,1,%count%) do (
-    echo %%i. !foldername[%%i]!
+    echo %%i. !folder[%%i]!
 )
 echo.
 echo Options:
@@ -27,7 +31,7 @@ echo 1. Set all to "Belum"
 echo 2. Set all to "Done"
 echo 3. Set specific folder to "Done"
 echo 4. Set specific folder to "Belum"
-echo 5. Set range of boxes to "Done" or "Belum"
+echo 5. Set range of BOX folders to "Done" or "Belum"
 echo 6. Create batch BOX folders
 echo 7. Exit
 echo.
@@ -48,7 +52,7 @@ for /l %%i in (1,1,%count%) do (
 )
 echo All folders set to "Belum".
 pause
-goto MENU
+goto SCAN_FOLDER
 
 :SET_ALL_DONE
 for /l %%i in (1,1,%count%) do (
@@ -56,7 +60,7 @@ for /l %%i in (1,1,%count%) do (
 )
 echo All folders set to "Done".
 pause
-goto MENU
+goto SCAN_FOLDER
 
 :SET_SPECIFIC_DONE
 set /p num="Enter folder number to set as Done (1-%count%): "
@@ -73,7 +77,7 @@ if !num! lss 1 (
 ren "!folder[%num%]!" "!foldername[%num%]! (Done)"
 echo Folder !num! set to "Done".
 pause
-goto MENU
+goto SCAN_FOLDER
 
 :SET_SPECIFIC_BELUM
 set /p num="Enter folder number to set as Belum (1-%count%): "
@@ -90,21 +94,11 @@ if !num! lss 1 (
 ren "!folder[%num%]!" "!foldername[%num%]! (Belum)"
 echo Folder !num! set to "Belum".
 pause
-goto MENU
+goto SCAN_FOLDER
 
 :SET_RANGE
-set /p start="Enter start range (1-%count%): "
-set /p end="Enter end range (1-%count%): "
-if !start! gtr %count% (
-    echo Invalid start range!
-    pause
-    goto MENU
-)
-if !end! gtr %count% (
-    echo Invalid end range!
-    pause
-    goto MENU
-)
+set /p start="Enter start BOX number (e.g., 1 for BOX 1): "
+set /p end="Enter end BOX number (e.g., 5 for BOX 5): "
 if !start! lss 1 (
     echo Invalid start range!
     pause
@@ -120,28 +114,42 @@ echo 1. Set range to "Done"
 echo 2. Set range to "Belum"
 set /p range_choice="Choose status for range (1-2): "
 
-if "!range_choice!"=="1" (
-    for /l %%i in (!start!,1,!end!) do (
-        ren "!folder[%%i]!" "!foldername[%%i]! (Done)"
+set "processed=0"
+for /l %%i in (1,1,%count%) do (
+    set "current_folder=!foldername[%%i]!"
+    for /f "tokens=2" %%n in ("!current_folder!") do (
+        set "box_num=%%n"
+        if !box_num! geq !start! if !box_num! leq !end! (
+            if "!range_choice!"=="1" (
+                ren "!folder[%%i]!" "!foldername[%%i]! (Done)"
+                set /a processed+=1
+            ) else if "!range_choice!"=="2" (
+                ren "!folder[%%i]!" "!foldername[%%i]! (Belum)"
+                set /a processed+=1
+            )
+        )
     )
-    echo Range !start! to !end! set to "Done".
-) else if "!range_choice!"=="2" (
-    for /l %%i in (!start!,1,!end!) do (
-        ren "!folder[%%i]!" "!foldername[%%i]! (Belum)"
-    )
-    echo Range !start! to !end! set to "Belum".
+)
+if !processed! equ 0 (
+    echo No BOX folders found in range BOX !start! to BOX !end!.
 ) else (
-    echo Invalid choice!
+    if "!range_choice!"=="1" (
+        echo Range BOX !start! to BOX !end! set to "Done" for !processed! folders.
+    ) else if "!range_choice!"=="2" (
+        echo Range BOX !start! to BOX !end! set to "Belum" for !processed! folders.
+    ) else (
+        echo Invalid choice!
+    )
 )
 pause
-goto MENU
+goto SCAN_FOLDER
 
 :CREATE_BATCH_BOX
 cls
 echo Create Batch BOX Folders
 echo.
-set /p start="Enter start range: "
-set /p end="Enter end range: "
+set /p start="Enter start BOX number: "
+set /p end="Enter end BOX number: "
 if !start! lss 1 (
     echo Start range must be 1 or greater!
     pause
