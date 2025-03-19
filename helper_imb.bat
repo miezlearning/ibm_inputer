9a1b719fb3abf9d595d0eeb39beef6db6a580a5c
miezlearning <gottfriedemptiness@gmail.com>
2025-03-19 14:21:34 +0800

    feat(helper-script): localize folder management script prompts to Indonesian

--- helper_imb.bat
+++ helper_imb.bat
@@ -2,40 +2,40 @@
 setlocal EnableDelayedExpansion
 
 :SCAN_FOLDER
-echo Scanning folders...
+echo Memindai folder...
 set "count=0"
 for /d %%i in (BOX*) do (
     set /a count+=1
     set "folder[!count!]=%%i"
     :: Bersihkan suffix untuk foldername
     set "clean_name=%%i"
-    set "clean_name=!clean_name: (Belum)=!"
-    set "clean_name=!clean_name: (Done)=!"
+    set "clean_name=!clean_name: (Sudah Diinput eArsip)=!"
+    set "clean_name=!clean_name: (Belum Diinput eArsip)=!"
     set "foldername[!count!]=!clean_name!"
 )
 
 if %count% equ 0 (
-    echo No BOX folders found in current directory.
+    echo Tidak ada folder BOX ditemukan di direktori ini.
 )
 
 :MENU
 cls
-echo Found %count% BOX folders:
+echo Ditemukan %count% folder BOX:
 echo.
 for /l %%i in (1,1,%count%) do (
     echo %%i. !folder[%%i]!
 )
 echo.
-echo Options:
-echo 1. Set all to "Belum"
-echo 2. Set all to "Done"
-echo 3. Set specific folder to "Done"
-echo 4. Set specific folder to "Belum"
-echo 5. Set range of BOX folders to "Done" or "Belum"
-echo 6. Create batch BOX folders
-echo 7. Exit
+echo Pilihan:
+echo 1. Setel semua ke "Belum Diinput eArsip"
+echo 2. Setel semua ke "Sudah Diinput eArsip"
+echo 3. Setel folder tertentu ke "Sudah Diinput eArsip"
+echo 4. Setel folder tertentu ke "Belum Diinput eArsip"
+echo 5. Setel rentang folder BOX ke "Sudah Diinput eArsip" atau "Belum Diinput eArsip"
+echo 6. Buat folder BOX secara batch
+echo 7. Keluar
 echo.
-set /p choice="Enter your choice (1-7): "
+set /p choice="Masukkan pilihan Anda (1-7): "
 
 if "%choice%"=="1" goto SET_ALL_BELUM
 if "%choice%"=="2" goto SET_ALL_DONE
@@ -48,71 +48,71 @@ goto MENU
 
 :SET_ALL_BELUM
 for /l %%i in (1,1,%count%) do (
-    ren "!folder[%%i]!" "!foldername[%%i]! (Belum)"
+    ren "!folder[%%i]!" "!foldername[%%i]! (Belum Diinput eArsip)"
 )
-echo All folders set to "Belum".
+echo Semua folder disetel ke "Belum Diinput eArsip".
 pause
 goto SCAN_FOLDER
 
 :SET_ALL_DONE
 for /l %%i in (1,1,%count%) do (
-    ren "!folder[%%i]!" "!foldername[%%i]! (Done)"
+    ren "!folder[%%i]!" "!foldername[%%i]! (Sudah Diinput eArsip)"
 )
-echo All folders set to "Done".
+echo Semua folder disetel ke "Sudah Diinput eArsip".
 pause
 goto SCAN_FOLDER
 
 :SET_SPECIFIC_DONE
-set /p num="Enter folder number to set as Done (1-%count%): "
+set /p num="Masukkan nomor folder untuk disetel ke Sudah Diinput eArsip (1-%count%): "
 if !num! gtr %count% (
-    echo Invalid number!
+    echo Nomor tidak valid!
     pause
     goto MENU
 )
 if !num! lss 1 (
-    echo Invalid number!
+    echo Nomor tidak valid!
     pause
     goto MENU
 )
-ren "!folder[%num%]!" "!foldername[%num%]! (Done)"
-echo Folder !num! set to "Done".
+ren "!folder[%num%]!" "!foldername[%num%]! (Sudah Diinput eArsip)"
+echo Folder !num! disetel ke "Sudah Diinput eArsip".
 pause
 goto SCAN_FOLDER
 
 :SET_SPECIFIC_BELUM
-set /p num="Enter folder number to set as Belum (1-%count%): "
+set /p num="Masukkan nomor folder untuk disetel ke Belum Diinput eArsip (1-%count%): "
 if !num! gtr %count% (
-    echo Invalid number!
+    echo Nomor tidak valid!
     pause
     goto MENU
 )
 if !num! lss 1 (
-    echo Invalid number!
+    echo Nomor tidak valid!
     pause
     goto MENU
 )
-ren "!folder[%num%]!" "!foldername[%num%]! (Belum)"
-echo Folder !num! set to "Belum".
+ren "!folder[%num%]!" "!foldername[%num%]! (Belum Diinput eArsip)"
+echo Folder !num! disetel ke "Belum Diinput eArsip".
 pause
 goto SCAN_FOLDER
 
 :SET_RANGE
-set /p start="Enter start BOX number (e.g., 1 for BOX 1): "
-set /p end="Enter end BOX number (e.g., 5 for BOX 5): "
+set /p start="Masukkan nomor BOX awal (misal, 1 untuk BOX 1): "
+set /p end="Masukkan nomor BOX akhir (misal, 5 untuk BOX 5): "
 if !start! lss 1 (
-    echo Invalid start range!
+    echo Rentang awal tidak valid!
     pause
     goto MENU
 )
 if !end! lss !start! (
-    echo End range must be greater than or equal to start range!
+    echo Rentang akhir harus lebih besar atau sama dengan rentang awal!
     pause
     goto MENU
 )
 
-echo 1. Set range to "Done"
-echo 2. Set range to "Belum"
-set /p range_choice="Choose status for range (1-2): "
+echo 1. Setel rentang ke "Sudah Diinput eArsip"
+echo 2. Setel rentang ke "Belum Diinput eArsip"
+set /p range_choice="Pilih status untuk rentang (1-2): "
 
 set "processed=0"
 for /l %%i in (1,1,%count%) do (
@@ -121,24 +121,24 @@ for /l %%i in (1,1,%count%) do (
         set "box_num=%%n"
         if !box_num! geq !start! if !box_num! leq !end! (
             if "!range_choice!"=="1" (
-                ren "!folder[%%i]!" "!foldername[%%i]! (Done)"
+                ren "!folder[%%i]!" "!foldername[%%i]! (Sudah Diinput eArsip)"
                 set /a processed+=1
             ) else if "!range_choice!"=="2" (
-                ren "!folder[%%i]!" "!foldername[%%i]! (Belum)"
+                ren "!folder[%%i]!" "!foldername[%%i]! (Belum Diinput eArsip)"
                 set /a processed+=1
             )
         )
     )
 )
 if !processed! equ 0 (
-    echo No BOX folders found in range BOX !start! to BOX !end!.
+    echo Tidak ada folder BOX ditemukan dalam rentang BOX !start! hingga BOX !end!.
 ) else (
     if "!range_choice!"=="1" (
-        echo Range BOX !start! to BOX !end! set to "Done" for !processed! folders.
+        echo Rentang BOX !start! hingga BOX !end! disetel ke "Sudah Diinput eArsip" untuk !processed! folder.
     ) else if "!range_choice!"=="2" (
-        echo Range BOX !start! to BOX !end! set to "Belum" for !processed! folders.
+        echo Rentang BOX !start! hingga BOX !end! disetel ke "Belum Diinput eArsip" untuk !processed! folder.
     ) else (
-        echo Invalid choice!
+        echo Pilihan tidak valid!
     )
 )
 pause
@@ -146,17 +146,17 @@ goto SCAN_FOLDER
 
 :CREATE_BATCH_BOX
 cls
-echo Create Batch BOX Folders
+echo Buat Folder BOX Secara Batch
 echo.
-set /p start="Enter start BOX number: "
-set /p end="Enter end BOX number: "
+set /p start="Masukkan nomor BOX awal: "
+set /p end="Masukkan nomor BOX akhir: "
 if !start! lss 1 (
-    echo Start range must be 1 or greater!
+    echo Rentang awal harus 1 atau lebih besar!
     pause
     goto MENU
 )
 if !end! lss !start! (
-    echo End range must be greater than or equal to start range!
+    echo Rentang akhir harus lebih besar atau sama dengan rentang awal!
     pause
     goto MENU
 )
@@ -164,11 +164,11 @@ if !end! lss !start! (
 for /l %%i in (!start!,1,!end!) do (
     mkdir "BOX %%i" 2>nul
     if errorlevel 1 (
-        echo Folder "BOX %%i" already exists or creation failed.
+        echo Folder "BOX %%i" sudah ada atau gagal dibuat.
     ) else (
-        echo Created "BOX %%i".
+        echo Membuat "BOX %%i".
     )
 )
-echo Batch BOX creation completed.
+echo Pembuatan folder BOX batch selesai.
 pause
 goto SCAN_FOLDER
